0c374fa79af2307907bb085f6e32ce32f3eb5224
Alberto-Mainardi <albertomainardi.12@gmail.com>
2024-05-07 18:53:35 -0300

    primeros datos de artículos en json y un script para insertar sus datos en html

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let contenido = document.querySelector(".root");
+fetch('./json/articulos.json').then((response) => response.json())
+.then((data) => {
+    let articulos = data.articulos;
+    articulos.forEach(articulo => {
+        contenido.innerHTML += `
+        <article id="${articulo.id}" style="max-width: 400px;" class="p-3 m-2 border border-2 col-lg-2">
+        <div class="d-flex align-items-center justify-content-center" style="min-height: 200px;">
+        <img class="w-75 h-100" src="${articulo.imagen}" alt="${articulo.alt} style="object-fit: cover;">
+        </div>
+        <h4 class="text-justify" style="text-overflow: ellipsis; white-space:nowrap; overflow: hidden;">${articulo.nombre}</h4>
+        <div class="d-flex justify-content-between my-4">
+            <h3>$${articulo.precio}</h3>
+            <button class="btn btn-primary">Comprar</button>
+        </div>
+        <p class="text-justify"><strong>Descripción</strong> <br>
+        ${articulo.descripcion}
+        </p>
+    </article> 
+        `;
+    });
+})
+.catch((error) => console.error("No se pudo conseguir la data:", error))
