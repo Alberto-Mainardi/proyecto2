5a34ff313cdd557c37c9c110abac0038c14d0a19
Alberto-Mainardi <albertomainardi.12@gmail.com>
2024-05-07 22:03:04 -0300

    Cambio de color de fondo a los articles de la página principl

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-let contenido = document.querySelector(".root");
+let recientes = document.querySelector("#recientes");
 fetch('./json/articulos.json').then((response) => response.json())
 .then((data) => {
     let articulos = data.articulos;
     articulos.forEach(articulo => {
-        contenido.innerHTML += `
-        <article id="${articulo.id}" style="max-width: 400px;" class="p-3 m-2 border border-2 col-lg-2">
+        recientes.innerHTML += `
+        <article id="${articulo.id}" style="max-width: 400px; background-color: white; border-radius: 1rem" class="p-3 m-2 border border-2 col-lg-2">
         <div class="d-flex align-items-center justify-content-center" style="min-height: 200px;">
         <img class="w-75 h-100" src="${articulo.imagen}" alt="${articulo.alt} style="object-fit: cover;">
         </div>
